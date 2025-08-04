beeddc4fb0530d5bb809b1a9a6a2bfd868e13550
Fatemeh <fatemeh.ganbari1993@gmail.com>
2025-08-04 20:32:42 +0330

    restructure folders

--- backend/src/BitBalance.API/Program.cs
+++ backend/src/BitBalance.API/Program.cs
@@ -21,6 +21,8 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+app.MapFallbackToFile("index.html");
+
 app.UseSerilogRequestLogging();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
